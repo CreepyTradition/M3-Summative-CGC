fc909979f44a230edc01bc336632451a0b53909a
Perkin Abrantes <perkinabrantes@gmail.com>
2024-03-14 21:36:31 +0800

    Final Touches

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 import * as THREE from './three.module.js';
 import { FlyControls } from './FlyControls.js';
+import { GLTFLoader } from './GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -50,8 +51,7 @@ function particles() {
 }
 
 function changeColor() {
-  let randomColor = Math.random() * 0xffffff;
-  stars.material.color.setHex(randomColor);
+  stars.material.color.setHex(0xC2B280);
 }
 
 function animateParticles() {
@@ -62,7 +62,6 @@ function animateParticles() {
       stars.position.z = 100;
     }
   }
-
 // Add lights
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.05);
 scene.add(ambientLight);
@@ -77,10 +76,10 @@ floorLight.position.set(0, 1.6, 0);
 scene.add(floorLight);
 
 // Sphere light geometry
-const sphereLightGeometry = new THREE.SphereGeometry(0.1, 16, 16); // Adjust radius and segments as needed
+const sphereLightGeometry = new THREE.SphereGeometry(1, 16, 16); // Adjust radius and segments as needed
 const sphereLightMaterial = new THREE.MeshLambertMaterial({ color: 0xffff00 }); // Adjust material properties as needed
 const sphereLightMesh = new THREE.Mesh(sphereLightGeometry, sphereLightMaterial);
-sphereLightMesh.position.set(0, 1.3, -4); // Position the sphere light
+sphereLightMesh.position.set(0, 40, -30); // Position the sphere light
 scene.add(sphereLightMesh);
 
 // Light for the sphere light
@@ -89,32 +88,91 @@ sphereLight.position.copy(sphereLightMesh.position); // Position the point light
 scene.add(sphereLight);
 
 // Surfaces
-const sidewallGeometry = new THREE.BoxGeometry(0.2, 34, 24);
 const floorGeometry = new THREE.BoxGeometry(44, 10, 24);
+
+// 3D models
+const loader = new GLTFLoader();
+// Ramp barrel
+loader.load(
+  './assets/models/barrel/old_rusted_metal_barrel.glb',
+  function (gltf) {
+    const barrel1 = gltf.scene;
+    barrel1.position.set(15, -4, -20);
+    barrel1.rotation.set(0, 0, 0);
+    barrel1.scale.set(1, 1, 1);
+    scene.add(barrel1);
+  },
+  undefined,
+  function (error) {
+    console.error('Error loading GLTF model', error);
+  }
+);
+// Site barrel
+loader.load(
+  './assets/models/barrel/old_rusted_metal_barrel.glb',
+  function (gltf) {
+    const barrel2 = gltf.scene;
+    barrel2.position.set(-2, -4, -35);
+    barrel2.rotation.set(0, 0, 0);
+    barrel2.scale.set(1, 1, 1);
+    scene.add(barrel2);
+  },
+  undefined,
+  function (error) {
+    console.error('Error loading GLTF model', error);
+  }
+);
+// A site main crate
+loader.load(
+  './assets/models/crates/wooden_crate_-_wooden_box.glb',
+  function (gltf) {
+    const crate = gltf.scene;
+    crate.position.set(-6, -4, -27);
+    crate.rotation.set(0, 0, 0);
+    crate.scale.set(5, 5, 5);
+    scene.add(crate);
+  },
+  undefined,
+  function (error) {
+    console.error('Error loading GLTF model', error);
+  }
+);
+// A site smol crate
+loader.load(
+  './assets/models/crates/wooden_crate_-_wooden_box.glb',
+  function (gltf) {
+    const smol = gltf.scene;
+    smol.position.set(-3, -4, -27);
+    smol.rotation.set(0, 0, 0);
+    smol.scale.set(4, 4, 4);
+    scene.add(smol);
+  },
+  undefined,
+  function (error) {
+    console.error('Error loading GLTF model', error);
+  }
+);
 
 // Load texture image
 const textureLoader = new THREE.TextureLoader();
-const floortexture = textureLoader.load('./assets/textures/floor.jpg');
-const sidewalltexture = textureLoader.load('./assets/textures/sidewall.jpg');
+const floortexture = textureLoader.load('./assets/textures/dust2Ramp.jpg');
 const rampCTTexture = textureLoader.load('./assets/textures/dust2CT.jpg');
 const aLongTexture = textureLoader.load('./assets/textures/dust2Ramp.jpg');
 const aSiteFloor = textureLoader.load('./assets/textures/dust2ASite.jpg');
 const dust2Wall = textureLoader.load('./assets/textures/dust2Wall.jpg');
+const CTspawn = textureLoader.load('./assets/textures/dust2CTSpawn.jpg');
+const rampWallTexture = textureLoader.load('./assets/textures/rampWall.jpg')
 
 // Create materials for surfaces
-const sidewallMaterial = new THREE.MeshStandardMaterial({ map: sidewalltexture });
 const floorMaterial = new THREE.MeshToonMaterial({ map: floortexture });
 
 // Create mesh for each surface
-const sidewallMesh = new THREE.Mesh(sidewallGeometry, sidewallMaterial);
 const floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
 
 // Position surfaces
-sidewallMesh.position.set(22.1, 6, -24);
 floorMesh.position.set(0, -16.5, -24);
 
 // Add surfaces to scene
-scene.add(sidewallMesh);
 scene.add(floorMesh);
 
 // Ramp to CT Spawn
@@ -124,7 +182,7 @@ const rampCTCube = new THREE.Mesh( rampCTGeo, rampCTMat );
 scene.add( rampCTCube );
 
 // A long
-const aLongGeo = new THREE.BoxGeometry( 20, 5, 10); 
+const aLongGeo = new THREE.BoxGeometry( 40, 5, 10); 
 const aLongMat = new THREE.MeshBasicMaterial( {map: aLongTexture} ); 
 const aLongCube = new THREE.Mesh( aLongGeo, aLongMat ); 
 scene.add( aLongCube );
@@ -153,6 +211,42 @@ const aSiteMat = new THREE.MeshBasicMaterial( {map: aSiteFloor} );
 const aSiteCube = new THREE.Mesh( aSiteGeo, aSiteMat ); 
 scene.add( aSiteCube );
 
+// CT Spawn
+const spawnGeo = new THREE.BoxGeometry( 14, 9, 0.5); 
+const spawnMat = new THREE.MeshBasicMaterial( {map: CTspawn} ); 
+const spawnCube = new THREE.Mesh( spawnGeo, spawnMat ); 
+scene.add( spawnCube );
+
+// Goose Wall
+const goosewallGeo = new THREE.BoxGeometry( 17, 20, 27); 
+const goosewallMat = new THREE.MeshBasicMaterial( {map: dust2Wall} ); 
+const goosewallCube = new THREE.Mesh( goosewallGeo, goosewallMat ); 
+scene.add( goosewallCube );
+
+// Short Wall
+const shorttopwallGeo = new THREE.BoxGeometry( 30, 20, 0.1); 
+const shorttopwallMat = new THREE.MeshBasicMaterial( {map: dust2Wall} ); 
+const shorttopwallCube = new THREE.Mesh( shorttopwallGeo, shorttopwallMat ); 
+scene.add( shorttopwallCube );
+
+// Long Wall
+const longwallGeo = new THREE.BoxGeometry( 20, 30, 35); 
+const longwallMat = new THREE.MeshBasicMaterial( {map: dust2Wall} ); 
+const longwallCube = new THREE.Mesh( longwallGeo, longwallMat ); 
+scene.add( longwallCube );
+
+// Ramp Wall
+const rampwallGeo = new THREE.BoxGeometry( 1, 20, 15); 
+const rampwallMat = new THREE.MeshBasicMaterial( {map: rampWallTexture} ); 
+const rampwallCube = new THREE.Mesh( rampwallGeo, rampwallMat ); 
+scene.add( rampwallCube );
+
+// Short
+const shortGeo = new THREE.BoxGeometry( 30, 20, 8); 
+const shortMat = new THREE.MeshBasicMaterial( {map: aSiteFloor} ); 
+const shortCube = new THREE.Mesh( shortGeo, shortMat ); 
+scene.add( shortCube );
+
 // Dust 2 site lower walls
 const lowerWallGeo = new THREE.BoxGeometry( 0.5, 13, 19); // Inline CT Spawn
 const lowerWallMat = new THREE.MeshBasicMaterial( {map: dust2Wall} ); 
@@ -166,24 +260,35 @@ const lowerWallCube2 = new THREE.Mesh( lowerWallGeo2, lowerWallMat2 );
 const lowerWallGeo3 = new THREE.BoxGeometry( 26, 13, 0.5); //Facing A Ramp
 const lowerWallMat3 = new THREE.MeshBasicMaterial( {map: dust2Wall} ); 
 const lowerWallCube3 = new THREE.Mesh( lowerWallGeo3, lowerWallMat3 ); 
+const shortWallGeo = new THREE.BoxGeometry( 14, 2, 0.5); //Short Wall
+const shortWallMat = new THREE.MeshBasicMaterial( {map: dust2Wall} ); 
+const shortWallCube = new THREE.Mesh( shortWallGeo, shortWallMat ); 
 scene.add( lowerWallCube );
 scene.add( lowerWallCube1 );
 scene.add( lowerWallCube2 );
 scene.add( lowerWallCube3 );
+scene.add( shortWallCube );
 
-// Position and Rotation for Ramps and other passageways
+// Position and Rotation for objects
 rampCTCube.position.set(-14.5, -13, -33.5);
 rampCTCube.rotation.set(-0.2, 0, 0);
-aLongCube.position.set(-12, -10.5, -17);
+aLongCube.position.set(-22, -10.5, -17);
 aRampCube.position.set(5, -10, -17);
 aRampCube.rotation.set(0, 0, 0.2);
 aRampGooseCube.position.set(18, -9, -17);
 gooseCube.position.set(6, -9, -24.5);
-lowerWallCube.position.set(-8, -9.6, -37);
+lowerWallCube.position.set(-8, -9.6, -36.5);
 lowerWallCube1.position.set(-9, -9.6, -27.3);
 lowerWallCube2.position.set(-10.2, -9.6, -24.3);
 lowerWallCube3.position.set(3, -9.6, -21.3);
 aSiteCube.position.set(7, -9, -36.5);
+spawnCube.position.set(-15, -9, -45.5);
+goosewallCube.position.set(13, 6, -40);
+shortCube.position.set(-7, -14, -50);
+shortWallCube.position.set(-15, -4, -45.5);
+shorttopwallCube.position.set(-7, 6, -54);
+longwallCube.position.set(-32, 1, -40);
+rampwallCube.position.set(22, 6, -20);
 
 function animate() {
 
@@ -195,3 +300,7 @@ function animate() {
 }
 
 animate();
+
+
+// "Wooden Crate - Wooden Box" (https://skfb.ly/opQpV) by Erdogan Taskurt is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+// "Old rusted Metal Barrel" (https://skfb.ly/o7POF) by Be-Ru is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
